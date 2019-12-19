774c5091c9afb27f6857890c5abe8e8ef802f220
giggin <46052932+giggin@users.noreply.github.com>
2019-12-19 14:39:12 +0100

    Add files via upload

--- settings_migrator.js
+++ settings_migrator.js
@@ -1,142 +1,142 @@
 const DefaultSettings = {
-    "enabled":  true,
-    "alerted":  true, // Pop-up notification
-    "notice":   true, // Pop-up notification
-    "messager": true, // Message
-    "marker":   true, // Lootbeam
-    "itemId":  98260, // Vergo's Head
-    "bosses": [
-        {huntingZoneId: 1023, templateId:     3000, name: "(Event)Dakuryon's Shade"},
-        {huntingZoneId: 1023, templateId: 20150805, name: "(Event)Dakuryon's Shade"},
-        {huntingZoneId: 1023, templateId: 88888888, name: "(Event)Big Blue Box"},
-        {huntingZoneId: 1023, templateId: 88888889, name: "(Event)Big Blue Box"},
-        {huntingZoneId: 1023, templateId:   160341, name: "(Event)Phoney Santa"},
-        {huntingZoneId: 1023, templateId: 99999997, name: "(Event)Fat Santa"},
-        {huntingZoneId: 1023, templateId: 99999998, name: "(Event)Tiny Santa"},
-        {huntingZoneId: 1023, templateId: 99999991, name: "(Event)Caiman Egg Thief"},
-        {huntingZoneId: 1023, templateId: 99999992, name: "(Event)Caiman Egg Thief"},
-        {huntingZoneId: 1023, templateId: 99999999, name: "(Event)Caiman Egg Thief"},
-        {huntingZoneId:  183, templateId:     6002, name: "(Event)AirDrop"},
+	"enabled":  true,
+	"alerted":  true, // Pop-up notification
+	"notice":   true, // Pop-up notification
+	"messager": true, // Message
+	"marker":   true, // Lootbeam
+	"itemId":  98260, // Vergo's Head
+	"bosses": [
+		{huntingZoneId: 1023, templateId:     3000, name: "(Event)Dakuryon's Shade"},
+		{huntingZoneId: 1023, templateId: 20150805, name: "(Event)Dakuryon's Shade"},
+		{huntingZoneId: 1023, templateId: 88888888, name: "(Event)Big Blue Box"},
+		{huntingZoneId: 1023, templateId: 88888889, name: "(Event)Big Blue Box"},
+		{huntingZoneId: 1023, templateId:   160341, name: "(Event)Phoney Santa"},
+		{huntingZoneId: 1023, templateId: 99999997, name: "(Event)Fat Santa"},
+		{huntingZoneId: 1023, templateId: 99999998, name: "(Event)Tiny Santa"},
+		{huntingZoneId: 1023, templateId: 99999991, name: "(Event)Caiman Egg Thief"},
+		{huntingZoneId: 1023, templateId: 99999992, name: "(Event)Caiman Egg Thief"},
+		{huntingZoneId: 1023, templateId: 99999999, name: "(Event)Caiman Egg Thief"},
+		{huntingZoneId:  183, templateId:     6002, name: "(Event)AirDrop"},
 		{huntingZoneId: 1023, templateId:   200002, name: "(Event)Grumpy Santa"}
 		{huntingZoneId: 1023, templateId:   200003, name: "(Event)Tiny Santa"}
 /* ==== 亚伦南部 ======================================================================================== */
-        {huntingZoneId:   2, templateId: 1271, name: "Arcun"},
-        {huntingZoneId:   3, templateId: 1271, name: "Arcun"},
-        {huntingZoneId:   5, templateId: 1271, name: "Arcun"},
-        {huntingZoneId:   6, templateId: 1271, name: "Arcun"},
-        {huntingZoneId:   7, templateId: 1271, name: "Arcun"},
-        {huntingZoneId:   4, templateId: 1271, name: "Eteral"},
-        {huntingZoneId:   9, templateId: 1271, name: "Eteral"},
-        {huntingZoneId:  10, templateId: 1271, name: "Eteral"},
-        {huntingZoneId:  11, templateId: 1271, name: "Eteral"},
-        {huntingZoneId:  12, templateId: 1271, name: "Eteral"},
-        {huntingZoneId:  15, templateId: 1271, name: "Foretta"},
-        {huntingZoneId:  16, templateId: 1271, name: "Foretta"},
-        {huntingZoneId:  17, templateId: 1271, name: "Foretta"},
-        {huntingZoneId:  23, templateId: 1271, name: "Foretta"},
-        {huntingZoneId:  18, templateId: 1271, name: "Viadu"},
-        {huntingZoneId:  19, templateId: 1271, name: "Viadu"},
-        {huntingZoneId:  21, templateId: 1271, name: "Viadu"},
-        {huntingZoneId:  24, templateId: 1271, name: "Viadu"},
+		{huntingZoneId:   2, templateId: 1271, name: "Arcun"},
+		{huntingZoneId:   3, templateId: 1271, name: "Arcun"},
+		{huntingZoneId:   5, templateId: 1271, name: "Arcun"},
+		{huntingZoneId:   6, templateId: 1271, name: "Arcun"},
+		{huntingZoneId:   7, templateId: 1271, name: "Arcun"},
+		{huntingZoneId:   4, templateId: 1271, name: "Eteral"},
+		{huntingZoneId:   9, templateId: 1271, name: "Eteral"},
+		{huntingZoneId:  10, templateId: 1271, name: "Eteral"},
+		{huntingZoneId:  11, templateId: 1271, name: "Eteral"},
+		{huntingZoneId:  12, templateId: 1271, name: "Eteral"},
+		{huntingZoneId:  15, templateId: 1271, name: "Foretta"},
+		{huntingZoneId:  16, templateId: 1271, name: "Foretta"},
+		{huntingZoneId:  17, templateId: 1271, name: "Foretta"},
+		{huntingZoneId:  23, templateId: 1271, name: "Foretta"},
+		{huntingZoneId:  18, templateId: 1271, name: "Viadu"},
+		{huntingZoneId:  19, templateId: 1271, name: "Viadu"},
+		{huntingZoneId:  21, templateId: 1271, name: "Viadu"},
+		{huntingZoneId:  24, templateId: 1271, name: "Viadu"},
 /* ==== 夏拉南部 ======================================================================================== */
-        {huntingZoneId:  26, templateId: 1271, name: "Ezart"},
-        {huntingZoneId:  27, templateId: 1271, name: "Ezart"},
-        {huntingZoneId:  28, templateId: 1271, name: "Ezart"},
-        {huntingZoneId:  29, templateId: 1271, name: "Ezart"},
-        {huntingZoneId:  30, templateId: 1271, name: "Versa"},
-        {huntingZoneId:  31, templateId: 1271, name: "Storan"},
-        {huntingZoneId:  32, templateId: 1271, name: "Storan"},
-        {huntingZoneId:  34, templateId: 1271, name: "Storan"},
-        {huntingZoneId:  35, templateId: 1271, name: "Viace"},
-        {huntingZoneId:  36, templateId: 1271, name: "Viace"},
-        {huntingZoneId:  38, templateId: 1271, name: "Viace"},
+		{huntingZoneId:  26, templateId: 1271, name: "Ezart"},
+		{huntingZoneId:  27, templateId: 1271, name: "Ezart"},
+		{huntingZoneId:  28, templateId: 1271, name: "Ezart"},
+		{huntingZoneId:  29, templateId: 1271, name: "Ezart"},
+		{huntingZoneId:  30, templateId: 1271, name: "Versa"},
+		{huntingZoneId:  31, templateId: 1271, name: "Storan"},
+		{huntingZoneId:  32, templateId: 1271, name: "Storan"},
+		{huntingZoneId:  34, templateId: 1271, name: "Storan"},
+		{huntingZoneId:  35, templateId: 1271, name: "Viace"},
+		{huntingZoneId:  36, templateId: 1271, name: "Viace"},
+		{huntingZoneId:  38, templateId: 1271, name: "Viace"},
 		{huntingZoneId:  1023, templateId: 8000, name: "Caiman Wanderer"},
-        {huntingZoneId:  40, templateId: 1271, name: "Vaneva"},
-        {huntingZoneId:  41, templateId: 1271, name: "Vaneva"},
+		{huntingZoneId:  40, templateId: 1271, name: "Vaneva"},
+		{huntingZoneId:  41, templateId: 1271, name: "Vaneva"},
 /* ==== 夏拉北部 ======================================================================================== */
-        {huntingZoneId:  45, templateId: 1271, name: "Lotica"},
-        {huntingZoneId:  47, templateId: 1271, name: "Lotica"},
-        {huntingZoneId:  48, templateId: 1271, name: "Hecurn"},
-        {huntingZoneId:  49, templateId: 1271, name: "Locarnum"},
-        {huntingZoneId:  50, templateId: 1271, name: "Locarnum"},
-        {huntingZoneId:  51, templateId: 1271, name: "Loahcun"},
-        {huntingZoneId:  52, templateId: 1271, name: "Loahcun"},
-        {huntingZoneId:  54, templateId: 1271, name: "Silvette"},
-        {huntingZoneId:  55, templateId: 1271, name: "Silvette"},
-        {huntingZoneId:  56, templateId: 1271, name: "Silvette"},
-        {huntingZoneId:  57, templateId: 1271, name: "Silvette"},
+		{huntingZoneId:  45, templateId: 1271, name: "Lotica"},
+		{huntingZoneId:  47, templateId: 1271, name: "Lotica"},
+		{huntingZoneId:  48, templateId: 1271, name: "Hecurn"},
+		{huntingZoneId:  49, templateId: 1271, name: "Locarnum"},
+		{huntingZoneId:  50, templateId: 1271, name: "Locarnum"},
+		{huntingZoneId:  51, templateId: 1271, name: "Loahcun"},
+		{huntingZoneId:  52, templateId: 1271, name: "Loahcun"},
+		{huntingZoneId:  54, templateId: 1271, name: "Silvette"},
+		{huntingZoneId:  55, templateId: 1271, name: "Silvette"},
+		{huntingZoneId:  56, templateId: 1271, name: "Silvette"},
+		{huntingZoneId:  57, templateId: 1271, name: "Silvette"},
 /* ===== 亚伦北部 ======================================================================================= */
-        {huntingZoneId: 172, templateId: 1271, name: "Varrek"},
-        {huntingZoneId: 181, templateId: 1271, name: "Varrek"},
-        {huntingZoneId: 182, templateId: 1271, name: "Varrek"},
-        {huntingZoneId: 183, templateId: 1278, name: "Varrek"},
-        {huntingZoneId: 191, templateId: 1271, name: "Varrek"},
+		{huntingZoneId: 172, templateId: 1271, name: "Varrek"},
+		{huntingZoneId: 181, templateId: 1271, name: "Varrek"},
+		{huntingZoneId: 182, templateId: 1271, name: "Varrek"},
+		{huntingZoneId: 183, templateId: 1278, name: "Varrek"},
+		{huntingZoneId: 191, templateId: 1271, name: "Varrek"},
 /* ==== 保護領地 ======================================================================================== */
-        {huntingZoneId:  13, templateId: 1271, name: "Vardung"},
+		{huntingZoneId:  13, templateId: 1271, name: "Vardung"},
 /* ==== 直辖領地 ======================================================================================== */
-        {huntingZoneId:  63, templateId: 1278, name: "Veracun"},
-        {huntingZoneId:  72, templateId: 1278, name: "Alluman"},
-        {huntingZoneId:  84, templateId: 1278, name: "Kaidera"},
+		{huntingZoneId:  63, templateId: 1278, name: "Veracun"},
+		{huntingZoneId:  72, templateId: 1278, name: "Alluman"},
+		{huntingZoneId:  84, templateId: 1278, name: "Kaidera"},
 /* ==== 神秘商店 ======================================================================================== */
-        {huntingZoneId:  63, templateId: 1271, name: "Petam"},
-        {huntingZoneId:  63, templateId: 1279, name: "Petam"},
-        {huntingZoneId:  72, templateId: 1271, name: "Hemusk"},
-        {huntingZoneId:  84, templateId: 1271, name: "Kaylight"},
-        {huntingZoneId: 183, templateId: 1271, name: "Jondo"},
+		{huntingZoneId:  63, templateId: 1271, name: "Petam"},
+		{huntingZoneId:  63, templateId: 1279, name: "Petam"},
+		{huntingZoneId:  72, templateId: 1271, name: "Hemusk"},
+		{huntingZoneId:  84, templateId: 1271, name: "Kaylight"},
+		{huntingZoneId: 183, templateId: 1271, name: "Jondo"},
 /* ==== 交付哥布林 ====================================================================================== */
-        {huntingZoneId:  63, templateId: 1276, logTime: 0, name: "Starc"},
-        {huntingZoneId:  63, templateId: 1284,             name: "Starc"},
-        {huntingZoneId:  84, templateId: 1276, logTime: 0, name: "Starc"},
-        {huntingZoneId:  72, templateId: 1276, logTime: 0, name: "Starc"},
-        {huntingZoneId: 183, templateId: 1276, logTime: 0, name: "Starc"},
+		{huntingZoneId:  63, templateId: 1276, logTime: 0, name: "Starc"},
+		{huntingZoneId:  63, templateId: 1284,             name: "Starc"},
+		{huntingZoneId:  84, templateId: 1276, logTime: 0, name: "Starc"},
+		{huntingZoneId:  72, templateId: 1276, logTime: 0, name: "Starc"},
+		{huntingZoneId: 183, templateId: 1276, logTime: 0, name: "Starc"},
 /* ==== 世界BOSS ======================================================================================== */
-        {huntingZoneId:  26, templateId: 5001, logTime: 0, name: "Ortan"},
-        {huntingZoneId:  39, templateId:  501, logTime: 0, name: "Hazard"},
-        {huntingZoneId:  51, templateId: 4001, logTime: 0, name: "Cerrus"},
+		{huntingZoneId:  26, templateId: 5001, logTime: 0, name: "Ortan"},
+		{huntingZoneId:  39, templateId:  501, logTime: 0, name: "Hazard"},
+		{huntingZoneId:  51, templateId: 4001, logTime: 0, name: "Cerrus"},
 /* ==== 空岛稀有 ======================================================================================== */
-        {huntingZoneId: 2020, templateId: 1100, name: "Supreme Draakon Pursuer"},
-        {huntingZoneId: 2020, templateId: 1101, name: "Draakon Guard-Captain"},
-        {huntingZoneId: 2020, templateId: 1102, name: "Supreme Draakon Dominator"},
-        {huntingZoneId: 2020, templateId: 1200, name: "Elite Naga Priest"},
-        {huntingZoneId: 2020, templateId: 1201, name: "Elite Naga Knight"},
-        {huntingZoneId: 2020, templateId: 1202, name: "Elite Naga Executioner"},
-        {huntingZoneId: 2020, templateId: 1300, name: "Manuk"},
-        {huntingZoneId: 2020, templateId: 1400, name: "Axcellob"},
-        {huntingZoneId: 2020, templateId: 1500, name: "Zemces"},
-        {huntingZoneId: 2020, templateId: 1600, name: "Red-Flecked Crab"},
-        {huntingZoneId: 2020, templateId: 1601, name: "Thorny Eyestalker"},
-        {huntingZoneId: 2020, templateId: 1700, name: "Vekes"}        
-    ]
+		{huntingZoneId: 2020, templateId: 1100, name: "Supreme Draakon Pursuer"},
+		{huntingZoneId: 2020, templateId: 1101, name: "Draakon Guard-Captain"},
+		{huntingZoneId: 2020, templateId: 1102, name: "Supreme Draakon Dominator"},
+		{huntingZoneId: 2020, templateId: 1200, name: "Elite Naga Priest"},
+		{huntingZoneId: 2020, templateId: 1201, name: "Elite Naga Knight"},
+		{huntingZoneId: 2020, templateId: 1202, name: "Elite Naga Executioner"},
+		{huntingZoneId: 2020, templateId: 1300, name: "Manuk"},
+		{huntingZoneId: 2020, templateId: 1400, name: "Axcellob"},
+		{huntingZoneId: 2020, templateId: 1500, name: "Zemces"},
+		{huntingZoneId: 2020, templateId: 1600, name: "Red-Flecked Crab"},
+		{huntingZoneId: 2020, templateId: 1601, name: "Thorny Eyestalker"},
+		{huntingZoneId: 2020, templateId: 1700, name: "Vekes"}        
+	]
 };
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-    if (from_ver === undefined) {
-        // Migrate legacy config file
-        return Object.assign(Object.assign({}, DefaultSettings), settings);
-    } else if (from_ver === null) {
-        // No config file exists, use default settings
-        return DefaultSettings;
-    } else {
-        // Migrate from older version (using the new system) to latest one
-        if (from_ver + 1 < to_ver) { // Recursively upgrade in one-version steps
-            settings = MigrateSettings(from_ver, from_ver + 1, settings);
-            return MigrateSettings(from_ver + 1, to_ver, settings);
-        }
-        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
-        // a switch for each version step that upgrades to the next version. This enables us to
-        // upgrade from any version to the latest version without additional effort!
-        switch (to_ver) {
-            default:
-                let oldsettings = settings
-                settings = Object.assign(DefaultSettings, {});
-                for (let option in oldsettings) {
-                    if (option == "bosses") continue
-                    if (settings[option]) {
-                        settings[option] = oldsettings[option]
-                    }
-                }
-                break;
-        }
-        return settings;
-    }
+	if (from_ver === undefined) {
+		// Migrate legacy config file
+		return Object.assign(Object.assign({}, DefaultSettings), settings);
+	} else if (from_ver === null) {
+		// No config file exists, use default settings
+		return DefaultSettings;
+	} else {
+		// Migrate from older version (using the new system) to latest one
+		if (from_ver + 1 < to_ver) { // Recursively upgrade in one-version steps
+			settings = MigrateSettings(from_ver, from_ver + 1, settings);
+			return MigrateSettings(from_ver + 1, to_ver, settings);
+		}
+		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
+		// a switch for each version step that upgrades to the next version. This enables us to
+		// upgrade from any version to the latest version without additional effort!
+		switch (to_ver) {
+			default:
+				let oldsettings = settings
+				settings = Object.assign(DefaultSettings, {});
+				for (let option in oldsettings) {
+					if (option == "bosses") continue
+					if (settings[option]) {
+						settings[option] = oldsettings[option]
+					}
+			}
+			break;
+		}
+		return settings;
+	}
 }
